80d1f4f74c27b2e133fa7a341bc9276add7b80e5
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-09-05 08:19:13 +0530

    y

--- mock12/r1/Productofarrayexceptself.js
+++ mock12/r1/Productofarrayexceptself.js
@@ -54,3 +54,30 @@
 // arr[3] = nums[1]*nums[2]*nums[4] = 8
 
 // arr[4] = nums[1]*nums[2]*nums[3] = 6
+
+
+
+let arr = [1,2,3,4]
+let N=arr.length
+// [24,12,8,6]
+const ProductOfArray=(arr,N)=>{
+    let left=[]
+    let right=[]
+    let ans=[]
+    let prod=1
+    for(let i=0;i<N;i++){
+        left.push(prod*=arr[i])
+    }
+    prod=1
+    for(let i=N-1;i>=0;i--){
+        right.push(prod*=arr[i])
+    }
+    right.reverse()
+    ans[0]=right[1]
+    ans[N-1]=left[N-2]
+    for(let i=1;i<N-1;i++){
+        ans[i]=(left[i-1]*right[i+1])
+    }
+    console.log(ans.join(" "));
+}
+ProductOfArray(arr,N)
